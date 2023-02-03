059cbbc80dbc953fec23d5eef4b0b06bb6374c18
wyk <wangyk071@chinatelecom.cn>
2023-02-03 14:18:18 +0800

    增加get_vm_image_info.py

---
+++ get_vm_image_info.py
@@ -0,0 +1,34 @@
+from oslo_concurrency import processutils
+import json
+import sys
+import os
+import pandas as pd
+
+(out, err) = processutils.execute("openstack", "server", "list", '--all', "-c", "Name", "-c", "ID", "-f", "json")
+all_vm_info = json.loads(out)
+vm_dict_list = list()
+for i in all_vm_info:
+    (out, err) = processutils.execute("openstack", "server", "show", i['ID'], "-c", "volumes_attached", "-f", "json")
+    vm_vol = json.loads(out)
+    # 从镜像启动的虚拟机，其volumes_attached为空列表
+    if vm_vol['volumes_attached'] == []:
+        i['root_disk_id'] = ''
+        (out, err) = processutils.execute("openstack", "server", "show", i['ID'], "-c", "image", "-f", "json")
+        vm_image = json.loads(out)
+        i['image'] = vm_image['image']
+    # 从镜像卷或者卷启动的虚拟机
+    else:
+        i['root_disk_id'] = vm_vol['volumes_attached'][0]['id']
+        (out, err) = processutils.execute("openstack", "volume", "show", i['root_disk_id'], "-c", "volume_image_metadata", "-f", "json", check_exit_code=[0,1])
+        # 当out != ''时，说明是从镜像卷启动的虚拟机
+        if out != '':
+            vol_image_info = json.loads(out)
+            i['image'] = vol_image_info['volume_image_metadata']['image_name']
+        # 当out == ''时，说明是从普通的卷启动的虚拟机
+        else:
+            i['image'] = ''
+    vm_dict_list.append(i)
+    print(i)
+df = pd.DataFrame.from_dict(vm_dict_list) 
+df.to_csv (r'vm_image.csv', index = False, header=True)
+
